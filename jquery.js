6ee5aff724f9564eaa48723f5c5840b4d7828ebe
hat_lover <jobin25051999@gmail.com>
2020-05-24 14:36:40 +0530

    first commit

---
+++ jquery.js
@@ -0,0 +1,19 @@
+
+
+$(document).ready(function () {
+    $(document).click(
+        function (event) {
+            var target = $(event.target);
+            var _mobileMenuOpen = $(".navbar-collapse").hasClass("show");
+            if (_mobileMenuOpen === true && !target.hasClass("navbar-toggler")) {
+                $("button.navbar-toggler").click();
+            }
+        }
+    );
+});
+
+
+
+$(document).ready(function(){
+    $('[data-toggle="popover"]').popover();   
+  });
